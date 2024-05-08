485ef210c16e6ca2d627c5389162d638f2771328
mrheltic <vincenzopioflorio@gmail.com>
2024-05-08 15:29:10 +0200

    Improved output

--- stop.sh
+++ stop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Stop the container
-docker stop eis-container || docker rm eis-container
+docker stop eis-container
 
 # Print a nicer output
 echo "Container stopped successfully."
+
+# Remove the container
+docker rm eis-container
+
+# Print a nicer output
+echo "Container removed successfully."
