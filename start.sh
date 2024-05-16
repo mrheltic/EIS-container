1b91c0359c7c88d1b25c477cd16562221039ada7
mrheltic <vincenzopioflorio@gmail.com>
2024-05-16 17:55:36 +0200

    Added IP port

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run --gpus all -d -p 8888:8888 --name eis-container eis-container
+docker run --gpus all -d -p 192.168.208.211:8888:8888 --name eis-container eis-container
 
 echo "EIS container is running."
-echo "You can access it at http://localhost:8888"
+echo "You can access it at http://192.168.208.201:8888"
